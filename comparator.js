6d75553910fb61a93f80273a213a1e9ad9d128ad
Juan Morales <crispamares@gmail.com>
2012-04-20 01:17:08 +0200

    * demo finished

--- comparator.js
+++ comparator.js
@@ -4,9 +4,20 @@ function compare(amount, el) {
     /**
      * Those references should be got from the backend
      */
-    var references = [{count: 9, 
+    var references = [{count: 18, 
 		       icon: "http://trac.opencoin.org/trac/opencoin/export/343/trunk/sandbox/jhb/mobile/icons/coins.svg",
 	               head: 'stolen money',
+		       classed: 'stolen',
+	               desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '
+			     + 'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
+			     + ' laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in '
+			     + 'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat'
+			     + ' non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' 
+		      },
+		      {count: 7, 
+		       icon: "http://trac.opencoin.org/trac/opencoin/export/343/trunk/sandbox/jhb/mobile/icons/coins.svg",
+	               head: 'fiscal fraud',
+		       classed: 'fraud',
 	               desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt '
 			     + 'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'
 			     + ' laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in '
@@ -45,8 +56,11 @@ var ComparatorView = Backbone.View.extend({
 	var w = args.width || this.$el.width();
 	var h = args.height || w / 2;
 	this.margin = args.margin || {top: 10, right: 10, bottom: 20, left: 10}; // top right bottom left
+	this.sideMargin = 20;
+	this.intraReferencesMargin = 10;
 	this.width = w - this.margin.right - this.margin.left,
 	this.height = h - this.margin.top - this.margin.bottom;
+	this.sideWidth = this.width/2 - this.sideMargin/2;
 
 	var template = '';
 	var compiled = _.template(template);
@@ -66,7 +80,7 @@ var ComparatorView = Backbone.View.extend({
 
 	this.gReferences = this.g.append('svg:g')
 	    .attr("class", "references")
-	    .attr("transform", "translate(" + this.width / 2 + "," + 0 + ")");
+	    .attr("transform", "translate(" + (this.sideWidth + this.sideMargin) + "," + 0 + ")");
 
     },
     render: function(){
@@ -85,10 +99,10 @@ var ComparatorView = Backbone.View.extend({
 
 	x = d3.scale.linear()
 	    .domain([0, xCount])
-	    .range([0, (this.width/2) / xCount]);
+	    .range([0, this.sideWidth]);
 	y = d3.scale.linear()	    
 	    .domain([0, yCount])
-	    .range([0, this.height/ yCount]);
+	    .range([0, this.height]);
 	
 
 	var rectAmount = this.gAmount.selectAll('rect.amount')
@@ -96,7 +110,6 @@ var ComparatorView = Backbone.View.extend({
 	rectAmount.enter()
 	    .append('svg:rect')
 	    .classed('amount', true)
-	    .style('title', String)
 	    .attr('x', this.width / 4)
 	    .attr('y', this.height / 2)
 	    .attr('rx', '15px')
@@ -106,8 +119,8 @@ var ComparatorView = Backbone.View.extend({
 	   .transition()
 	    .attr('x', 0)
 	    .attr('y', 0)
-	    .attr('width', 0.9 * this.width/2)
-	    .attr('height', 0.9 * this.height);
+	    .attr('width', this.sideWidth)
+	    .attr('height', this.height);
 	rectAmount.enter().append("svg:title")
             .text(function(d) { return ''+ d + ' €'; });
 
@@ -118,14 +131,22 @@ var ComparatorView = Backbone.View.extend({
 	    .data(referenceData);
 	rectReference.enter()
 	    .append('svg:rect')
-	    .classed('amount', true)
-	    .style('title', String)
-	    .attr('x', function(d, i) {return x(Math.round(i / xCount));})
+	    .attr('class', function(d,i) {return d.classed;})
+	    .classed('reference', true)
+ 	    .attr('x', function(d, i) {return x(Math.floor(i / xCount)) + iconSize/2;})
+	    .attr('y', function(d, i) {return y(i % yCount) + iconSize/2;})
+	    .attr('rx', '15px')
+	    .attr('ry', '15px')
+	    .attr('width', 0)
+	    .attr('height', 0)
+	   .transition()
+	    .delay(function(d,i) {return i*20;})
+	    .attr('x', function(d, i) {return x(Math.floor(i / xCount));})
 	    .attr('y', function(d, i) {return y(i % yCount);})
-//	    .attr('rx', '15px')
-//	    .attr('ry', '15px')
 	    .attr('width', iconSize)
 	    .attr('height', iconSize);
+	rectReference.enter().append("svg:title")
+            .text(function(d) { return d.head; });
 
 	
     }
